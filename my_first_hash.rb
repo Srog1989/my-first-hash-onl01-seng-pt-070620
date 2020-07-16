ab67d84202814312bac7a2a395fc261c382a0a67
Sarah Rogers <s.rogers0715@yahoo.com>
2020-07-16 01:20:25 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,7 +4,7 @@ end
 
 
 def shipping_manifest
-  the_manifet= {"whale bone corsets" => "5", "porcelain vases"=> "2", "oil paintings" => "3"}
+  the_manifet= {"whale bone corsets" => 5, "porcelain vases"=> 2, "oil paintings" => 3}
 end
 
 def retrieval
