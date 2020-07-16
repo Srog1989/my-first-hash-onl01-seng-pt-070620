0808aeb3c17b630a57e557524a15114c01c86130
Sarah Rogers <s.rogers0715@yahoo.com>
2020-07-16 01:32:47 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,6 +22,7 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-                 shipping_manifest["muskets"] = "2".muskets
-                 shipping_manifest["gun powder"] = "4". gun_powder
+                 shipping_manifest["muskets"] = "2"
+                 shipping_manifest["gun powder"] = "4"
+                 shipping_manifest
 end
